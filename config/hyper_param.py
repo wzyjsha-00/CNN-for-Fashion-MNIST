a57d69a717102b308928352e5cf57627bd2e625c
wzyjsha <wzyjsha@163.com>
2022-06-12 21:35:58 +0800

    adjust the hyper parameters and file paths

--- config/hyper_param.py
+++ config/hyper_param.py
@@ -4,6 +4,6 @@ weight_decay = 1e-4
 step_size = 7
 gamma = 0.1
 batch_size = 8
-num_epochs = 2
+num_epochs = 30
 num_workers = 4
 num_classes = 10
--- config/path.py
+++ config/path.py
@@ -8,16 +8,6 @@ train_label_file = os.path.abspath(os.path.join(main_path, 'data', "Fashion-MNIS
 test_data_folder = os.path.abspath(os.path.join(main_path, 'data', "Fashion-MNIST", 'fashion_test_jpg_10000'))
 test_label_file = os.path.abspath(os.path.join(main_path, 'data', "Fashion-MNIST", 'fashion_test_jpg_10000.txt'))
 
-
-data_folder_path = os.path.abspath(os.path.join(main_path, "data"))
-image_folder_path = os.path.abspath(os.path.join(main_path, "data", "images"))
-label_file_path = os.path.abspath(os.path.join(main_path, "data", "labels.csv"))
-
 model_folder_path = os.path.abspath(os.path.join(main_path, "model"))
-
-pre_trained_resnet34 = os.path.abspath(os.path.join(main_path, "model", "pre_trained", "resnet34-b627a593.pth"))
-pre_trained_resnet50 = os.path.abspath(os.path.join(main_path, "model", "pre_trained", "resnet50-0676ba61.pth"))
-pre_trained_resnet101 = os.path.abspath(os.path.join(main_path, "model", "pre_trained", "resnet101-5d3b4d8f.pth"))
-pre_trained_resnet101_2 = os.path.abspath(os.path.join(main_path, "model", "pre_trained", "resnext101_20.pth"))
 
 train_log_path = os.path.abspath(os.path.join(main_path, "model", "train.log"))
